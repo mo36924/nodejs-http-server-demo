c8f4b14ac9b4ebcbd0e029285c6f5514d650f301
mo36924 <mo36924@users.noreply.github.com>
2021-11-20 18:25:20 +0900

    ♻️

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { createServer } from "http";
 
+const port = process.env.PORT || 8080;
+
 createServer((_req, res) => {
-  const now = new Date().toJSON();
-  console.log(now);
-  res.end(now);
-}).listen(process.env.PORT || 8080);
+  const data = `port:${port}\ndate:${new Date().toJSON()}`;
+  console.log(data);
+  res.end(data);
+}).listen(port);
